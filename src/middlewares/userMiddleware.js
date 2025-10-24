18ccd18c8d4ef7ef7264dfe7a73325d5b93742f3
KevLex <kevinparra507@gmail.com>
2025-10-24 17:39:56 -0500

    Creacion de middleware de validación para usuarios, freelancers, contratos, propuestas y ofertas

---
+++ src/middlewares/userMiddleware.js
@@ -0,0 +1,13 @@
+
+import { validationResult } from 'express-validator';
+
+export default function validateUserResults(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            ok: false,
+            errors: errors.array().map(e => ({ field: e.param, msg: e.msg }))
+        });
+    }
+    next();
+}
